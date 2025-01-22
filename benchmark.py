851d483ba4856832be72f7982ce7bca354ab4a77
Nick John Eliopoulos <nickjeliopoulosdev@gmail.com>
2025-01-22 05:30:45 -0500

    Working on adding benchmarking and reporting

--- benchmark.py
+++ benchmark.py
@@ -4,6 +4,7 @@ from einops import einsum
 from diffusers import DiffusionPipeline
 from diffusers.utils import pt_to_pil
 from evotorch.algorithms import CMAES, SNES, CEM
+from evotorch.logging import PandasLogger
 from diffusion_pt import diffusion_sample
 from fitness_fn import *
 from noise_injection import rotational_transform
@@ -17,6 +18,7 @@ import logging
 import warnings
 from pathlib import Path, PosixPath, WindowsPath
 from tqdm.auto import tqdm
+import pandas
 
 
 ###
@@ -48,8 +50,8 @@ def parse_args() -> argparse.Namespace:
 
 	parser.add_argument('--prompt', type=str, default="A picture of a dog.")
 	parser.add_argument('--cache-dir', type=str, default=None)
-	parser.add_argument('--denoising-steps', type=int, default=20)
-	parser.add_argument('--sample-count', type=int, default=12)
+	parser.add_argument('--denoising-steps', type=int, default=50)
+	parser.add_argument('--sample-count', type=int, default=20)
 	parser.add_argument('--guidance-scale', type=float, default=7.5)
 
 	parser.add_argument('--solver-splits', type=int, default=2)
@@ -161,7 +163,7 @@ def compose_fitness_callables_and_weights(args: argparse.Namespace) -> Tuple[Lis
 
 
 ### Iteratively sample and search for a noise vector solution
-def diffusion_solve_and_sample(args : argparse.Namespace, solver: CMAES, sample_callable : Callable):
+def diffusion_solve_and_sample(args : argparse.Namespace, solver: CMAES, sample_callable : Callable) -> Tuple[torch.Tensor, pandas.DataFrame]:
 	### Get the frames as a single tensor of empty elements
 	sampled_frames = torch.empty(
 		(1 + args.sample_count, 3, 512, 512), 
@@ -169,7 +171,9 @@ def diffusion_solve_and_sample(args : argparse.Namespace, solver: CMAES, sample_
 		device=args.device
 	)
 
-	bench_logger.info("Sampling initial frames")
+	### Latency Report
+	latency_report_dict = {}
+	latency_report_columns = ["Sample Index", "Sample Time (ms)", "Solve Time (ms)"]
 
 	### Get the initial image
 	sampled_frames[0] = sample_callable()
@@ -190,14 +194,16 @@ def diffusion_solve_and_sample(args : argparse.Namespace, solver: CMAES, sample_
 		mean =  mean.to(args.device, dtype=args.pipeline_dtype).unsqueeze(-1).unsqueeze(-1)
 		rot = rot.to(args.device, dtype=args.pipeline_dtype)
 
-		# rot = rot.permute(0, 3, 1, 2)
 		x = einsum(centroid, rot, 'b c h w, p c1 c -> p c1 h w')
 		x = x + mean_scale * mean - centroid
 
 		sampled_frames[1 + step] = sample_callable(x)
 
+	### Dict to DataFrame
+	latency_report_dataframe = pandas.DataFrame(data=latency_report_dict, columns=latency_report_columns)
+
 	### Return values
-	return sampled_frames
+	return sampled_frames, latency_report_dataframe
 
 
 if __name__ == "__main__":
@@ -222,7 +228,7 @@ if __name__ == "__main__":
 
 	sample_callable, latents, _, dtype = diffusion_sample(
 		pipeline, 
-		prompt=["a picture of a dog"], 
+		prompt=[args.prompt], 
 		num_inference_steps=args.denoising_steps, 
 		generator=torch_rng, 
 		guidance_scale=args.guidance_scale, 
@@ -240,7 +246,7 @@ if __name__ == "__main__":
 	mean_scale = 0.01
 	initial_bounds = (-1,1)
 
-	objective_callable, objective_inner_callable, centroid, solution_length = rotational_transform(
+	objective_callable, objective_transform_callable, centroid, solution_length = rotational_transform(
 		sample_callable, 
 		total_fitness_callable, 
 		latents.shape, 
@@ -259,21 +265,26 @@ if __name__ == "__main__":
 		initialization=None)
 	
 	solver = CMAES(problem, stdev_init=1, separable=False, csa_squared=True)
+	solver_logger = PandasLogger(solver)
 
 	###
 	### Benchmarking!
 	###
-	sampled_frames_tensor = diffusion_solve_and_sample(args, solver, sample_callable)
-	bench_logger.info(f"Sampled frames shape: {sampled_frames_tensor.shape}")
+	sampled_frames_tensor, latency_report_dataframe = diffusion_solve_and_sample(args, solver, sample_callable)
+	solver_report_dataframe = solver_logger.to_dataframe()
 
 	### Convert `sampled_frames` Tensor to PIL Image list
-	### Plot and/or dump frames as necessary
 	sampled_frames_image_list = pt_to_pil(sampled_frames_tensor)
 
 	### Sanitize paths
 	frame_dump_path = Path("frames")
 	frame_dump_path.mkdir(parents=True, exist_ok=True)
 
-	ffmpeg_make_gif_subprocess(sampled_frames_image_list, frame_dump_path, "animation.gif", fps=2)
+	### Dump frames, GIF
+	ffmpeg_make_gif_subprocess(sampled_frames_image_list, frame_dump_path, "animation.gif", fps=2, dump_only=False)
+
+	### Dump solver report, latency
+	solver_report_dataframe.to_csv("report_solver.csv", index=False)
+	latency_report_dataframe.to_csv("report_latency.csv", index=False)
 
 	bench_logger.info("Done!")
