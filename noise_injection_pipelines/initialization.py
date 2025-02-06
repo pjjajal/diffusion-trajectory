3595e9ae02780aaea95a2d43cdb3a27282aa18ca
pjjajal <jajalpurvish@gmail.com>
2025-02-05 23:04:19 -0500

    SD3 working. Added initialization stuff and updated noise_injection.

---
+++ noise_injection_pipelines/initialization.py
@@ -0,0 +1,12 @@
+import torch
+
+def rotation_initalization(values, solution_length, latents_shape, stdev=0.1):
+    _, c, h, w = latents_shape
+    identity = torch.eye(c, device=values.device, dtype=values.dtype).reshape(1, -1)
+    values[:, :solution_length - c] = identity
+    values +=  torch.randn(values.shape, device=values.device, dtype=values.dtype) * stdev
+    return values
+
+def randn_intialization(values, stdev=1):
+    values = torch.randn_like(values)
+    return values
