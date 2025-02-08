e8b5d983954f4da5a6d774cef8692dfb1af7fad7
Nick John Eliopoulos <nickjeliopoulosdev@gmail.com>
2025-02-07 19:18:45 -0500

    Added dummy script for debugging whether fitness implementations differ numerically. WIll remove once rewards_zoo is replaced with our fitness functions

--- fitness_equivalence_checker.py
+++ fitness_equivalence_checker.py
@@ -16,39 +16,37 @@ if __name__ == "__main__":
 	parser.add_argument('--cache-dir', type=str, default="D://Huggingface")
 	args = parser.parse_args()
 
-	processor = AutoProcessor.from_pretrained(
-		"yuvalkirstain/PickScore_v1", cache_dir=args.cache_dir
-	)
+	# processor = AutoProcessor.from_pretrained(
+	# 	"yuvalkirstain/PickScore_v1", cache_dir=args.cache_dir
+	# )
 
-	print(processor)
+	# print(processor)
 
-	# prompt = "A photo of a dog"
-	# device = torch.device("cuda:0")
-	# dtype = torch.float32
+	prompt = "A photo of a dog"
+	device = torch.device("cuda:0")
+	dtype = torch.float32
 
-	# dummy_image = Image.open(args.image_path)
-	# dummy_image = torch.tensor(np.array(dummy_image)).permute(2, 0, 1).unsqueeze(0).float().to(device)
+	dummy_image = Image.open(args.image_path)
+	dummy_image = torch.tensor(np.array(dummy_image), device=device, dtype=dtype, requires_grad=True).permute(2, 0, 1).unsqueeze(0)
 
-	# ### Input image checker
-	# print(f"Input image shape: {dummy_image.shape}")
-	# print(f"Input image max/min: {dummy_image.max()}/{dummy_image.min()}")
+	### Input image checker
+	print(f"Input image shape: {dummy_image.shape}")
+	print(f"Input image max/min: {dummy_image.max()}/{dummy_image.min()}")
 
-	# with torch.inference_mode():
-	# 	### PickScore
-	# 	# our_fitness_callable = pickscore_fitness_fn(prompt, cache_dir=HF_CACHE, device=device, dtype=dtype).item()
-	# 	# dno_fitness_callable = pick_loss_fn(device, dtype)
+	with torch.inference_mode():
+		### PickScore
+		our_fitness_callable = pickscore_fitness_fn(prompt, cache_dir=args.cache_dir, device=device, dtype=dtype)
+		dno_fitness_callable = pick_loss_fn(device=device, inference_dtype=dtype)
 
+		# our_fitness_callable = aesthetic_fitness_fn(cache_dir=args.cache_dir, device=device, dtype=dtype)
+		# dno_fitness_callable = aesthetic_loss_fn(device=device, inference_dtype=dtype)
 
-	# 	our_fitness_callable = aesthetic_fitness_fn(cache_dir=args.cache_dir, device=device, dtype=dtype)
-	# 	dno_fitness_callable = aesthetic_loss_fn(device, dtype)
+		our_fitness = our_fitness_callable(dummy_image)
+		dno_fitness = -dno_fitness_callable(dummy_image, prompt)
 
+		# our_fitness = 0
+		# dno_fitness = 0
 
-	# 	our_fitness = our_fitness_callable(dummy_image)
-	# 	dno_fitness = -dno_fitness_callable(dummy_image, prompt)
-
-	# 	# our_fitness = 0
-	# 	# dno_fitness = 0
-
-	# 	print(f"Fitness: {our_fitness} (ours) vs. {dno_fitness} (DNO)")
+		print(f"Fitness: {our_fitness} (ours) vs. {dno_fitness} (DNO)")
 
 	exit(0)
