71f0abf643ec933bf19291ccd4205a3c20603495
Nick John Eliopoulos <nickjeliopoulosdev@gmail.com>
2025-06-24 21:15:46 -0500

    Removed old wood_ulrich implementation. Minor change to vmf_test_minimal to include kappa as parameter

--- vmf_test_minimal.py
+++ vmf_test_minimal.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from vmf.vmf_nes import vMF_NES
-from vmf.wood_ulrich import sample_vmf_wood
+from vmf.wood_ulrich import sample_vmf_wood_v2 as sample_vmf_wood
 import optax
 from scipy.stats import vonmises_fisher
 from scipy.stats import gaussian_kde
@@ -39,42 +39,42 @@ def vmf_nes(popsize, solution_shape):
     return population
 
 
-def sample_vmf(popsize, key, mean):
+def sample_vmf(popsize: int, key: jax.Array, kappa: float | jax.Array, mean: jax.Array) -> jax.Array:
     x = sample_vmf_wood(
         key=key,
         mu=mean,
-        kappa=2.0,
+        kappa=kappa,
         n_samples=popsize
     )
 
     return x
 
-def sample_scipy_vmf(popsize, mean):
+def sample_scipy_vmf(popsize: int, kappa: int, mean: np.ndarray) -> np.ndarray:
     x = vonmises_fisher.rvs(
         mu=mean,
-        kappa=50.0,
+        kappa=kappa,
         size=popsize
     )
 
     return x
 
-
 if __name__ == "__main__":
     popsize = 128
     d = 64
+    kappa = 50
     solution_shape = (d,)
 
     key = jax.random.key(0)
     key, init_key = jax.random.split(key, 2)    
-
     mean = jax.random.normal(init_key, (1, *solution_shape))
 
-    x = sample_vmf(popsize, key, mean)
-    print(f"Sampled vMF: {x}")
-    print(f"Sample Norms: {jnp.linalg.norm(x, axis=-1)}")
+    x = sample_vmf(popsize, key, kappa, mean)
+
+    x_norm = jnp.linalg.norm(x, axis=-1)
+    print(f"Sample norms allclose to 1.0: {jnp.allclose(x_norm, 1.0)}")
     
     # mean = mean / jnp.linalg.norm(mean, axis=-1, keepdims=True)
-    # y = sample_scipy_vmf(popsize, np.array(mean).squeeze())
+    # y = sample_scipy_vmf(popsize, kappa, np.array(mean).squeeze())
 
     # print(x.shape)
     # print(y.shape)
